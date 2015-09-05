b22188449e84e3f3b957093b0484a122874c73c6
rabramowitz <Becky@sho-249.apn.wireless-pennnet.upenn.edu>
2015-09-05 11:09:10 -0400

    hi

---
+++ PennApps/PennApps/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PennApps
+//
+//  Created by Becky Abramowitz on 9/5/15.
+//  Copyright (c) 2015 Becky Abramowitz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
